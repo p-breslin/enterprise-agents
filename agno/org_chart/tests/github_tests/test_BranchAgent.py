de96fecbd980307ffdf71ddf96a03e5db63d4831
Peter <peter.breslin@experienceflow.ai>
2025-04-29 14:46:42 -0700

    Update test_BranchAgent.py

--- agno/org_chart/tests/github_tests/test_BranchAgent.py
+++ agno/org_chart/tests/github_tests/test_BranchAgent.py
@@ -1,56 +1,81 @@
-import json
 import logging
 import asyncio
+import pathlib
 from dotenv import load_dotenv
+from models.schemas import BranchList
 from agno.agent import RunResponse
 from agno.tools.mcp import MCPTools
 from agno.utils.pprint import pprint_run_response
-
-from agents import build_review_agent
-from utils.helpers import load_config, resolve_model
 from integrations.github_mcp import get_github_mcp_config
+
+from agents import build_branch_agent
+from utils.helpers import (
+    load_config,
+    resolve_model,
+    inject_state,
+    validate_output,
+    parse_json,
+)
 
 load_dotenv()
 log = logging.getLogger(__name__)
 
-# Runtime setup
-DEBUG = False
+
+# Runtime setup --------------------------------------------
+
+DEBUG = True
+PROVIDER = "google"
 CFG = load_config("runtime")
 ORG = CFG["GITHUB"]["org"]
-MODEL = resolve_model("openai", CFG["MODELS"]["openai"]["repo"])
+MODEL_ID = CFG["MODELS"][PROVIDER]["pr"]
+MODEL = resolve_model(PROVIDER, MODEL_ID)
+TEST_SESSION_ID = "test_session_BranchAgent"
+
+# Define Input (epics data from EpicAgent)
+INPUT_DIR = pathlib.Path(__file__).parent / "../test_data"
+INPUT_FILE = INPUT_DIR / "test_repo_data.json"
+INPUT_STATE_KEY = "input_repo_data"
+
+SAVENAME = f"test_BranchAgent_{MODEL_ID}.json"
+OUTPUT_DIR = pathlib.Path(__file__).parent / "../test_output"
+OUTPUT_DIR.mkdir(exist_ok=True)
+OUTPUT_FILE = OUTPUT_DIR / f"branch_output_{MODEL_ID}.json"
 
 # Docker command for MCP tools
 MCP_CMD, MCP_ENV = get_github_mcp_config()
 
-PR_NUMBER = 342
-pr_stub = {
-    "owner": "xpander-ai",
-    "repo": "xpander-sdk",
-    "pr_number": f"{PR_NUMBER}",
-    "head_sha": "98e01ad7c7c91e4388b297065b829dbf37dd99cc",
-}
-
-state_key = "prs_data_input"
-initial_state = {state_key: json.dumps({"pull_requests": [pr_stub]})}
-
-MCP_CMD, MCP_ENV = get_github_mcp_config()
+# ----------------------------------------------------------
 
 
-async def run_review_agent_test():
+async def run_branch_agent_test():
+    log.info("Starting BranchAgent test.")
+
+    # Load the input repo data and add cutoff_date
+    initial_state = inject_state(INPUT_FILE, INPUT_STATE_KEY)
+
     async with MCPTools(MCP_CMD, env=MCP_ENV) as mcp_tools:
-        agent = build_review_agent(
+        agent = build_branch_agent(
             model=MODEL,
             tools=[mcp_tools],
             initial_state=initial_state,
+            prompt="branch_prompt",
             debug=DEBUG,
         )
+
+        # Run agent
+        log.info("Running BranchAgent...")
+        trigger_message = "Fetch branches for the given repository"
         resp: RunResponse = await agent.arun(
-            f"Collect reviews for PR #{PR_NUMBER}",
-            session_id="review_agent_test",
+            trigger_message, session_id=TEST_SESSION_ID
         )
-        assert resp.content, "ReviewAgent returned empty content"
-        pprint_run_response(resp, markdown=True)
+        assert resp.content, "BranchAgent returned empty content"
+        pprint_run_response(resp)
+
+        # Validate and save output
+        outfile = OUTPUT_DIR / SAVENAME
+        json_obj = parse_json(resp.content)
+        validate_output(outfile, json_obj, BranchList)
 
 
 if __name__ == "__main__":
-    asyncio.run(run_review_agent_test())
+    asyncio.run(run_branch_agent_test())
